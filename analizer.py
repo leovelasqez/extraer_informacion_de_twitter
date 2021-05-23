362d28d16d6327d6a6a1a1a6d673a5ecaaf0d71b
leovelasqez <brayanvelasqez@gmail.com>
2021-05-23 18:37:03 -0500

    Programa finalizado

---
+++ analizer.py
@@ -0,0 +1,26 @@
+
+exclude_words = ['el', 'la', 'los', 'las', 'un', 'una', 'unos', 'unas', 'al', 'del', 'lo', 'le', 'y', 'e', 'o', 'u', 'de', 'a', 'en', 'que', 'es', 'por', 'para', 'con', 'se', 'su', 'les', 'me', 'q', 'te', 'pero', 'mi', 'ya', 'cuando', 'como', 'estoy', 'voy', 'porque', 'he', 'son', 'solo', 'tengo', 'muy']
+
+top_words = {}
+tweets_topic = open('./bitcoin.txt', encoding='utf-8')
+for line in tweets_topic:
+    words = line.strip().lower().split()
+    for word in words:
+        if word not in exclude_words:
+            top_words[word] = top_words.get(word, 0) + 1
+
+most_used_words = sorted(top_words, key=top_words.get, reverse=True)
+
+count_u = 0
+for word in most_used_words:
+    if count_u < 10 and word.startswith('@'):
+        print(top_words[word], word)
+        count_u += 1
+        
+count = 0
+for word in most_used_words:
+    if count < 20:
+        print(top_words[word], word)
+        count += 1
+
+print('*'*40) 
